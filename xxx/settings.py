d569655f67b76d2b74bc3760c09c763e5f27f856
liuzhibo666 <qq1203739431@sina.com>
2019-09-17 21:35:01 +0800

    再次修改

--- xxx/settings.py
+++ xxx/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Shanghai'
+LANGUAGE_CODE = ''
 
 TIME_ZONE = 'UTC'
 

1cc1bdc925bcf627a49bb89b65803eb7ad8e4843
liuzhibo666 <55428656+liuzhibo666@users.noreply.github.com>
2019-09-17 22:13:47 +0800

    Revert "Feature like"

--- app/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-def a(request):
-    return render(request,'xxx.html')
-# Create your views here.
--- xxx/settings.py
+++ xxx/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'Asia/Shanghai'
 
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
